9a13bb73c6fe04d51b35cd0fd9b6ea9a86595748
Hakan <hkaraoguz@yahoo.com.tr>
2017-01-30 11:39:33 +0100

    Rechanged the wait task as goto waypoint

--- src/har_observe_task/execute_scheduled_sweeps_cameraonly.py
+++ src/har_observe_task/execute_scheduled_sweeps_cameraonly.py
@@ -57,7 +57,8 @@ class HARTaskManager():
             self.goto_tasks.append(create_go_to_waypoint_task(waypoint))
 
         # wait at Charging Point middle of the corridor
-        self.wait_task = create_wait_action_task()
+        #self.wait_task = create_wait_action_task()
+        self.wait_task = create_go_to_waypoint_task("WayPoint1")
 
         try:
             rospy.wait_for_service(self.add_task_srv_name,timeout=10)
