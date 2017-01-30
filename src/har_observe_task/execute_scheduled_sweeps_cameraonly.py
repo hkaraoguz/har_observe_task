ad2b03e4957574b704d9318a9b92c3878e18b674
Hakan <hkaraoguz@yahoo.com.tr>
2017-01-30 15:42:26 +0100

    Fix bug for calling service

--- src/har_observe_task/execute_scheduled_sweeps_cameraonly.py
+++ src/har_observe_task/execute_scheduled_sweeps_cameraonly.py
@@ -100,7 +100,8 @@ class HARTaskManager():
             server = rospy.ServiceProxy('/deep_object_detection/detect_objects', DetectObjects)
             req = DetectObjectsRequest()
             req.images=self.images
-	    resp = server(req,0.85)
+            req.confidence_threshold=0.85
+	    resp = server(req)
         except rospy.ServiceException, e:
             self.images=[]
             print "Service call failed: %s"%e
