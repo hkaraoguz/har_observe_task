6fa588d4f7f5616a73da5ce26c2e14857c1634cf
Hakan <hkaraoguz@yahoo.com.tr>
2017-01-31 09:56:10 +0100

    Data logging has been added. Code commenting and refactoring

--- src/har_observe_task/execute_scheduled_sweeps_cameraonly.py
+++ src/har_observe_task/execute_scheduled_sweeps_cameraonly.py
@@ -31,36 +31,49 @@ class HARTaskManager():
         self.minutes=[]
         self.images = []
         self.current_task_id = -1
+        self.current_task_num = -1
         self.current_wait_task_id = -1
         self.previoustasktimeslot = -1
         self.should_update_model = 0
         self.person_count  = 0
         self.timer = None
         self.rosbag = None
+        self.waypoints = ['WayPoint19','WayPoint20','WayPoint23','WayPoint10']
+        self.placenames =['Office612','Office621','MeetingRoom','Kitchen']
+        self.goto_tasks = []
+
+        ''' Prepare the root path for data storage '''
         self.datarootdir =  os.path.expanduser('~') # Get the home directory
         self.datarootdir +="/harscheduling/"
 
         if not os.path.exists(self.datarootdir):
             os.makedirs(self.datarootdir)
 
+        self.logfilename = copy.deepcopy(self.datarootdir)
+        self.logfilename += datetime.now().strftime('%Y-%m-%d')
+        self.logfilename += ".txt"
+        f = open(self.logfilename, 'w')
+        f.close()
 
+        ''''''
+
+        ''' Services and message subscriptions '''
         self.add_task_srv_name = '/task_executor/add_task'
         self.set_exe_stat_srv_name = '/task_executor/set_execution_status'
         self.finished_sub = rospy.Subscriber('/local_metric_map/room_observations', RoomObservation, callback=self.finishedCB)
 
         sub = rospy.Subscriber("task_executor/events",TaskEvent,self.taskexecutorCB)
-
-        self.waypoints = ['WayPoint19','WayPoint20','WayPoint23','WayPoint10']
-
-        self.goto_tasks = []
+        ''''''
 
         for waypoint in self.waypoints:
             self.goto_tasks.append(create_go_to_waypoint_task(waypoint))
 
         # wait at Charging Point middle of the corridor
         #self.wait_task = create_wait_action_task()
-        self.wait_task = create_wait_action_task("WayPoint1")
-
+        ''' Create the charging task '''
+        self.wait_task = create_wait_action_task("ChargingPoint")
+        ''''''
+        ''' Check For Services '''
         try:
             rospy.wait_for_service(self.add_task_srv_name,timeout=10)
         except:
@@ -71,13 +84,31 @@ class HARTaskManager():
         except:
             rospy.logerr("Service not available!!")
             sys.exit(-1)
+        ''''''
+
+        self.create_timeslot_array()
+
 
         # make sure the executive is running -- this only needs to be done once for the whole system not for every task
-        #set_execution_status = rospy.ServiceProxy(self.set_exe_stat_srv_name, strands_executive_msgs.srv.SetExecutionStatus)
-        #set_execution_status(True)
+        set_execution_status = rospy.ServiceProxy(self.set_exe_stat_srv_name, strands_executive_msgs.srv.SetExecutionStatus)
+        set_execution_status(True)
         #self.sweep_tasks = create_har_sweep_tasks("journalExpKTH","roi","configHARRooms")
-        self.create_timeslot_array()
+
         #print self.sweep_tasks.keys()
+    def logdata(self,success,place="",person_count=0):
+        f = open(self.logfilename, 'a')
+        if f:
+            if success is 1:
+                s = "Success"+"\t"+datetime.now().strftime('%Y-%m-%d_%H:%M')+"\t"+place+"\t"+str(person_count)+"\n"
+            else:
+                s = "Fail"+"\t"+datetime.now().strftime('%Y-%m-%d_%H:%M')+"\n"
+            f.write(s)
+            f.close()
+        else:
+            rospy.logerr("Log file could not be opened")
+
+
+
     def timerCB(self,event):
         rospy.loginfo("Timer callback")
         self.observation_sub.unregister()
@@ -101,7 +132,7 @@ class HARTaskManager():
             req = DetectObjectsRequest()
             req.images=self.images
             req.confidence_threshold=0.85
-	    resp = server(req)
+            resp = server(req)
         except rospy.ServiceException, e:
             self.images=[]
             print "Service call failed: %s"%e
@@ -110,25 +141,35 @@ class HARTaskManager():
         if self.rosbag:
             rospy.loginfo("Writing into rosbag")
             self.rosbag.write('/head_xtion/rgb/image_rect_color',msg)
+
         person_objs = []
+
         for obj in resp.objects:
             if obj.label == "person" and obj.width*obj.height > 4096:
                 self.person_count +=1
                 person_objs.append(obj)
+
         if self.person_count > 2:
+
+            self.log_data(success=1,place=self.placenames[self.current_task_num],person_count=self.person_count)
+
             self.person_count = 0
+
             bridge = CvBridge()
             cv_image = bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
+
             for person_obj in person_objs:
                 cv2.rectangle(cv_image,(person_obj.x,person_obj.y),(person_obj.x+person_obj.width, person_obj.y+person_obj.height),color=(255,255,0),thickness=2)
                 text = "%.2f"%person_obj.confidence
                 cv2.putText(cv_image,text,(person_obj.x+person_obj.width/4,person_obj.y+person_obj.height/2),cv2.FONT_HERSHEY_SIMPLEX,0.7,color=(255,0,255),thickness=2)
+
             filename = copy.deepcopy(self.datarootdir)
             filename += datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
             filename += ".jpg"
             cv2.imwrite(filename,cv_image)
             self.images = []
             self.observation_sub.unregister()
+
             if self.timer:
                 self.timer.shutdown()
                 self.timer =None
@@ -166,7 +207,7 @@ class HARTaskManager():
         self.rosbag = rosbag.Bag(filename, 'w')
         self.person_count = 0
         self.observation_sub = rospy.Subscriber('/head_xtion/rgb/image_rect_color', Image, callback=self.observationCB)
-        self.timer = rospy.Timer(rospy.Duration(20), self.timerCB,oneshot=True)
+        self.timer = rospy.Timer(rospy.Duration(15), self.timerCB,oneshot=True)
 
             #sys.exit(-1)
     def taskexecutorCB(self,taskevent):
@@ -174,21 +215,17 @@ class HARTaskManager():
         print taskevent.event
         print taskevent.task.task_id
         print self.current_task_id
-        if taskevent.event > 11 and taskevent.event is not 16:
+        if taskevent.event > 9 and taskevent.event is not 16:
             if taskevent.task.task_id is self.current_task_id:
                 self.should_update_model = 0
+                self.logdata(success=1,)
                 self.current_wait_task_id=self.send_task(self.wait_task)
            # elif taskevent.task.task_id is self.current_wait_task_id:
            #     self.current_wait_task_id = self.send_task(self.wait_task)
-        if taskevent.event is 16 and taskevent.task.task_id is self.current_task_id:
-            #self.observation_sub = rospy.Subscriber('/head_xtion/rgb/image_rect_color', Image, callback=self.observationCB)
-            #filename = copy.deepcopy(self.datarootdir)
-            #filename += datetime.now().strftime('%Y-%m-%d_%H:%M')
-            #filename += ".bag"
-            #rospy.loginfo("Rosbag path: %s",filename)
-            #self.rosbag = rosbag.Bag(filename, 'w')
-            self.timer = rospy.Timer(rospy.Duration(10), self.startObservationCB,oneshot=True)
-            #self.current_wait_task_id=self.send_task(self.wait_task)
+        if taskevent.task.task_id is self.current_task_id:
+            if taskevent.event is 16:
+                self.timer = rospy.Timer(rospy.Duration(10), self.startObservationCB,oneshot=True)
+
 
 
 
@@ -257,9 +294,9 @@ if __name__ =="__main__":
 
     while not rospy.is_shutdown():
         if hartask_manager.check_timeslot():
-            taskno = randint(0,len(hartask_manager.goto_tasks)-1)
-            rospy.loginfo("Sending task with id %s",taskno)
-            hartask_manager.current_task_id = hartask_manager.send_task(hartask_manager.goto_tasks[taskno])
+            hartask_manager.current_task_num = randint(0,len(hartask_manager.goto_tasks)-1)
+            rospy.loginfo("Sending task with id %s",hartask_manager.current_task_num)
+            hartask_manager.current_task_id = hartask_manager.send_task(hartask_manager.goto_tasks[hartask_manager.current_task_num])
             #hartask_manager.current_wait_task_id = hartask_manager.send_task(hartask_manager.wait_task)
             #tasks.append(hartask_manager.sweep_tasks[key])
             #tasks.append(hartask_manager.wait_task)
